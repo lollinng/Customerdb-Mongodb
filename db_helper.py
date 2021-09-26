0301f27698cc0103d07e5739f3c7f94f06383456
lollinng <55660103+lollinng@users.noreply.github.com>
2021-09-27 00:10:43 +0530

    Add files via upload

---
+++ db_helper.py
@@ -0,0 +1,42 @@
+import pymongo
+
+class DBHELPER:
+    def __init__(self):
+        client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+        mydb = client['Employee']
+        global information 
+        information = mydb.employeeinformation       # table name
+
+    def insert_user(self,username,email,phone,products,shopping_value):
+        record = {
+            'name':username,
+            'email':email,
+            'phone':phone,
+            'products':products,
+            'shopping_value':shopping_value
+        }
+        information.insert_one(record)
+        print('user saved to db')
+
+    def fetch_all(self):
+        dataset = information.find({})
+        return dataset
+
+    def delete_user(self,email):
+        print(email)
+        information.delete_one(
+            { "email": email} 
+        )
+
+    def update_user(self,new_name, new_email, new_phone,new_products,new_shopping_value):
+        information.update_one(
+            {"name":new_name},
+                {"$set":{
+                    "name":new_name,
+                    "email":new_email,
+                    "phone":new_phone,
+                    'products':new_products,
+                    'shopping_value':new_shopping_value
+                }}
+        )
+        print("Userid {} updated !".format(new_name))
