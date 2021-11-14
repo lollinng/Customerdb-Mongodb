8bb83bdf4dc2e55d9446ec86b3aa64eb9890c827
lollinng <55660103+lollinng@users.noreply.github.com>
2021-11-14 21:37:49 +0530

    Update db_helper.py

--- db_helper.py
+++ db_helper.py
@@ -3,21 +3,37 @@ import pymongo
 class DBHELPER:
     def __init__(self):
         client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
-        mydb = client['Employee']
+        mydb = client['Customer_Database']
         global information 
-        information = mydb.employeeinformation       # table name
+        information = mydb.customerinformation       # table name
 
-    def insert_user(self,username,email,phone,products,shopping_value):
+    def insert_user(
+            self,username, email, phone,no_products,
+            product1,productname1,productprice1,category1,
+            product2,productname2,productprice2,category2,
+            product3,productname3,productprice3,category3 
+    ):  
+        
+        if productprice2 == '':
+            productprice2 = 0
+        if productprice3 == '':
+            productprice3 = 0
+
         record = {
             'name':username,
             'email':email,
             'phone':phone,
-            'products':products,
-            'shopping_value':shopping_value
+            'no_products':no_products,
+            'products':[
+                {'product':product1,'productname':productname1,'productprice':productprice1,'category':category1},
+                {'product':product2,'productname':productname2,'productprice':productprice2,'category':category2},
+                {'product':product3,'productname':productname3,'productprice':productprice3,'category':category3},
+                ],
+            'shopping_value': int(productprice1) + int(productprice2) + int(productprice3)
         }
         information.insert_one(record)
         print('user saved to db')
-        
+
     def fetch_user(self,email):
         for record in information.find({'email':email}):
             return record
@@ -32,15 +48,33 @@ class DBHELPER:
             { "email": email} 
         )
 
-    def update_user(self,new_name, new_email, new_phone,new_products,new_shopping_value):
+    def update_user(
+        self,username, email, phone,no_products,
+            product1,productname1,productprice1,category1,
+            product2,productname2,productprice2,category2,
+            product3,productname3,productprice3,category3,
+    ):
+    
+        if productprice2 == '':
+            productprice2 = 0
+            if productprice3 == '':
+                productprice3 = 0
+
         information.update_one(
-            {"name":new_name},
-                {"$set":{
-                    "name":new_name,
-                    "email":new_email,
-                    "phone":new_phone,
-                    'products':new_products,
-                    'shopping_value':new_shopping_value
-                }}
+            {"email":email},
+            {
+                "$set":{
+                    "name":username,
+                    "email":email,
+                    "phone":phone,
+                    'no_products':no_products,
+                    'products':[
+                        {'product':product1,'productname':productname1,'productprice':productprice1,'category':category1},
+                        {'product':product2,'productname':productname2,'productprice':productprice2,'category':category2},
+                        {'product':product3,'productname':productname3,'productprice':productprice3,'category':category3}
+                    ],
+                    'shopping_value' : int(productprice1) + int(productprice2) + int(productprice3)
+                }
+            }
         )
-        print("Userid {} updated !".format(new_name))
+        print("Userid {} updated !".format(username))
